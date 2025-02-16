7e1204092d751ef40bb3d6aa0a565a505b795a02
RashingPro <rashingpro@yandex.ru>
2025-02-16 18:39:23 +0300

    main.py now using as test file

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import datetime
-import threading
 
 from managers import google_table_manager
 
 
-def add_event(name: str, date: datetime.datetime, description: str = ''):
-    t1 = threading.Thread(target=g_table.add_event, args=(name, date, description))
-    t1.start()
-
-
 if __name__ == '__main__':
     g_table = google_table_manager.GoogleTableManager('./auth.json', debug=True)
-
+    g_table.add_event('тесмтовый', datetime.datetime.now(), 'тето')
