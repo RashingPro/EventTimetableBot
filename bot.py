885dcc22dcccbcc94cedb2df91523434b4b1ab68
RashingPro <rashingpro@yandex.ru>
2025-02-16 18:39:04 +0300

    Add /ping, /make_announcement, on_reaction event

--- bot.py
+++ bot.py
@@ -1,15 +1,23 @@
+import datetime
+
 import discord
 from discord.ext import commands
 
+from managers import google_table_manager
+
+ADMIN_ROLE_ID = 1309498655992188948
+INVISIBLE_TEXT = '||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||'
 
 with open('./config/token') as file:
     TOKEN = file.read()
-bot = commands.Bot(command_prefix='!', help_command=None)
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', help_command=None, intents=intents)
 
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+    print(f'Logged in as {bot.user.name}\nBot is ready')
 
 
 @bot.slash_command(name='ping', description='Узнать пинг бота')
@@ -20,5 +28,70 @@ async def cmd_ping(ctx: discord.ApplicationContext):
         description=f'Пинг: {round(bot.latency*1000)} ms'
     )
     await ctx.respond(embed=embed, ephemeral=True)
+
+
+@bot.slash_command(name='make_announcement', description='Создать анонс ивента')
+async def cmd_make_announcement(
+        ctx: discord.ApplicationContext,
+        title: discord.Option(str, name='название', required=True),
+        date: discord.Option(str, name='дата', required=True, description='формат дд.мм чч:мм'),
+        event_id: discord.Option(int, name='айди', required=True),
+        description: discord.Option(str, name='описание', required=False)
+):
+    if ADMIN_ROLE_ID not in [role.id for role in ctx.author.roles]:
+        embed = discord.Embed(
+            color=0xff0000,
+            title=f'❌ Недостаточно полномочий'
+        )
+        await ctx.respond(embed=embed, ephemeral=True)
+        return
+    date = datetime.datetime.strptime(date, '%d.%m %H:%M')
+    channel = ctx.channel
+    embed = discord.Embed(
+        color=0x00ff00,
+        title=f'Анон ивента "{title}"',
+        description=f'**Дата ивента:** {date.strftime('%d.%m %H:%M')}\n' +
+                    (f'**Описание:** {'\n'.join(description.split('\\n'))}' if description is not None else '')
+    )
+    await channel.send(content=f'{INVISIBLE_TEXT}{event_id};', embed=embed)
+    await ctx.delete()
+
+
+@bot.event
+async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
+    channel = bot.get_guild(payload.guild_id).get_channel(payload.channel_id)
+    message = await channel.fetch_message(payload.message_id)
+    if message.author.id != bot.user.id:
+        return
+    message_text = message.content
+    event_id = int(message_text.split('|')[-1].split(';')[0])
+    user = channel.guild.get_member(payload.user_id)
+    g_table = google_table_manager.GoogleTableManager('./auth.json', debug=True)
+    event = g_table.get_event(event_id)
+
+    if user.id in [x['user_id'] for x in event['members']]:
+        await user.create_dm()
+        dm_channel = user.dm_channel
+        embed = discord.Embed(
+            color=0xff0000,
+            title=f'Ошибка!',
+            description=f'Вы **уже записаны** на ивент {event['name']} '
+                        f'который пройдет **{event["date"].strftime("%d.%m в %H:%M")}**\n\n'
+                        f'Если вы передумали участвовать в ивенте, то напишите <@669760356448731139>'
+        )
+        await dm_channel.send(embed=embed)
+        return
+
+    g_table.book_user(event_id, payload.user_id, user.display_name, '')
+    await user.create_dm()
+    dm_channel = user.dm_channel
+    embed = discord.Embed(
+        color=0x0000ff,
+        title=f'Запись на ивент',
+        description=f'Вы записались на ивент **{event['name']}** '
+                    f'который пройдет **{event["date"].strftime("%d.%m в %H:%M")}**\n\n'
+                    f'Если вы передумали участвовать в ивенте, то напишите <@669760356448731139>'
+    )
+    await dm_channel.send(embed=embed)
 
 bot.run(TOKEN)
--- managers/google_table_manager.py
+++ managers/google_table_manager.py
@@ -1,4 +1,5 @@
 import datetime
+import threading
 
 import pygsheets
 from managers.config_manager import ConfigManager
@@ -24,43 +25,120 @@ class GoogleTableManager:
         return table
 
     def add_event(self, name: str, date: datetime.datetime, description: str = '', table_key: str = None):
-        if self.debug:
-            print('<GoogleTableManager> Adding event...')
-        if table_key is None:
-            table = self.get_main_table()
-        else:
-            table = self.get_table(table_key)
-        sheet = table.add_worksheet(f'{date.strftime(f'%d.%m (%a) %H:%M')} {name}')
-        sheet.update_value('A1', 'Информация')
-        sheet.cell('A1').set_text_format('bold', True)
-        sheet.merge_cells('A1', 'B1')
-        sheet.cell('A1').set_horizontal_alignment(pygsheets.HorizontalAlignment.CENTER)
+        def _add_event(self, name, date, description, table_key):
 
-        sheet.update_value('A2', 'Название')
-        sheet.update_value('B2', name)
+            if self.debug:
+                print(f'<GoogleTableManager> Adding event "{name}" at {date.strftime(f"%d.%m (%a) %H:%M")}...')
+            if table_key is None:
+                table = self.get_main_table()
+            else:
+                table = self.get_table(table_key)
+            sheet = table.add_worksheet(f'{date.strftime(f'%d.%m (%a) %H:%M')} {name}')
+            sheet.update_value('A1', 'Информация')
+            sheet.cell('A1').set_text_format('bold', True)
+            sheet.merge_cells('A1', 'B1')
+            sheet.cell('A1').set_horizontal_alignment(pygsheets.HorizontalAlignment.CENTER)
 
-        sheet.update_value('A3', 'Дата проведения')
-        sheet.update_value('B3', date.strftime(f'%d.%m %H:%M (%A)'))
+            sheet.update_value('A2', 'Название')
+            sheet.update_value('B2', name)
 
-        sheet.update_value('A4', 'Описание')
-        sheet.update_value('B4', description)
+            sheet.update_value('A3', 'Дата проведения')
+            sheet.update_value('B3', date.strftime(f'%d.%m %H:%M (%A)'))
 
-        drange = sheet.range('A1:B4', 'range')
-        drange.update_borders(
-            top=True,
-            right=True,
-            bottom=True,
-            left=True,
-            style='SOLID_MEDIUM'
-        )
-        drange.update_borders(inner_vertical=True, inner_horizontal=True, style='SOLID_THICK')
+            sheet.update_value('A4', 'Описание')
+            sheet.update_value('B4', description)
 
-        sheet.adjust_column_width(1)
-        sheet.adjust_column_width(2)
+            drange = sheet.range('A1:B4', 'range')
+            drange.update_borders(
+                top=True,
+                right=True,
+                bottom=True,
+                left=True,
+                style='SOLID_MEDIUM'
+            )
+            drange.update_borders(inner_vertical=True, inner_horizontal=True, style='SOLID_THICK')
 
-        sheet.update_value('D1', 'Участники')
-        sheet.cell('D1').set_text_format('bold', True)
-        sheet.cell('D1').set_horizontal_alignment(pygsheets.HorizontalAlignment.CENTER)
+            sheet.adjust_column_width(1)
+            sheet.adjust_column_width(2)
 
-        if self.debug:
-            print(f'<GoogleTableManager> Successfully added event "{name}"')
+            sheet.update_value('D1', 'Участники')
+            sheet.cell('D1').set_text_format('bold', True)
+            sheet.cell('D1').set_horizontal_alignment(pygsheets.HorizontalAlignment.CENTER)
+
+            if self.debug:
+                print(f'<GoogleTableManager> Successfully added event "{name}"')
+
+        t1 = threading.Thread(target=_add_event, args=(self, name, date, description, table_key))
+        t1.start()
+
+    def remove_event(self, event_id: int, table_key: str = None):
+        def _remove_event(self: GoogleTableManager, event_id, table_key):
+            if self.debug:
+                print(f'<GoogleTableManager> Removing event with id "{id}"...')
+            if table_key is None:
+                table = self.get_main_table()
+            else:
+                 table = self.get_table(table_key)
+
+            table.del_worksheet(table.worksheet('id', event_id))
+
+            if self.debug:
+                print(f'<GoogleTableManager> Successfully removed event with id "{id}"...')
+
+        t1 = threading.Thread(target=_remove_event, args=(self, event_id, table_key))
+        t1.start()
+
+    def get_event(self, event_id: int, table_key: str = None):
+        def _get_event(self: GoogleTableManager, event_id, table_key):
+            if self.debug:
+                print(f'<GoogleTableManager> Getting event with id "{event_id}"...')
+            if table_key is not None:
+                table = self.get_table(table_key)
+            else:
+                table = self.get_main_table()
+            sheet: pygsheets.Worksheet = table.worksheet('id', event_id)
+            name = sheet.cell('B2').value
+            date = datetime.datetime.strptime(sheet.cell('B3').value, '%d.%m %H:%M (%A)')
+            description = sheet.cell('B4').value
+
+            members = []
+            i = 2
+            val = sheet.cell((i, 4)).value
+            while val != '':
+                _memb = val.split('/')
+                members.append({'username': _memb[0], 'user_id': int(_memb[1]), 'comment': _memb[2]})
+                i += 1
+                val = sheet.cell((i, 4)).value
+
+            if self.debug:
+                print(f'<GoogleTableManager> Successfully got event with id "{event_id}"')
+
+            return {'name': name, 'date': date, 'description': description, 'members': members}
+
+        return _get_event(self, event_id, table_key)
+
+    def book_user(self, event_id: int, user_id: int, username: str, comment: str = '', table_key: str = None):
+        def _book_user(self: GoogleTableManager, event_id, user_id, username, comment, table_key):
+            if self.debug:
+                print(f'<GoogleTableManager> Booking user "{username}" ({user_id}) to event "{event_id}"')
+
+            if table_key is None:
+                table = self.get_main_table()
+            else:
+                table = self.get_table(table_key)
+            sheet = table.worksheet('id', event_id)
+
+            i = 2
+            val = sheet.cell((i, 4)).value
+            while val != '':
+                i += 1
+                val = sheet.cell((i, 4)).value
+
+            sheet.update_value((i, 4), f'{username}/{user_id}/{comment}')
+
+            if self.debug:
+                print(f'<GoogleTableManager> Successfully booked user "{username}" ({user_id}) to event "{event_id}"')
+
+        t1 = threading.Thread(target=_book_user, args=(self, event_id, user_id, username, comment, table_key))
+        t1.start()
+
